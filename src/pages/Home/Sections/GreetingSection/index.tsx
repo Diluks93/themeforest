ea661d5f30a3e6a5166dbbf9264dcddb207a48e1
Dzmitry Iliukovich <mr.ilyukovich@gmail.com>
2022-11-16 19:07:13 +0300

    mend

--- src/pages/Home/Sections/GreetingSection/index.tsx
+++ src/pages/Home/Sections/GreetingSection/index.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 
 import { PathsToPage, description } from 'constants/';
 import { ButtonCircle, Section } from 'components';
-import { RootState } from 'store';
 import { Mark } from 'styles';
 
 import mainTeamBackground from 'static/images/main-team.webp';
 import { ContainerStyled, DescriptionStyled, Title, Image } from './styled';
 
 export function GreetingSection() {
-  const { isPlay } = useSelector(({ player }: RootState) => player);
-
   return (
     <>
       <Section col>
         <ContainerStyled>
           <Title>
             Find true power in your data with
-            <Mark>Ensome {isPlay ? 'true' : 'false'}</Mark>
+            <Mark>Ensome</Mark>
           </Title>
           <DescriptionStyled>
             {description}
